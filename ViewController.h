939e6fbd3687ca770aa26ab86f8870d60e8b8e02
AjayGirolkar <ajaygirolkar@gmail.com>
2016-03-23 14:24:42 +0530

    Added files via upload

---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UltimatixLeaveApplication
+//
+//  Created by Ajay Girolkar on 12/02/16.
+//  Copyright (c) 2016 Tcs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
